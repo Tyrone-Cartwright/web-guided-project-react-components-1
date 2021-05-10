88fa0c631d0e8d014974508b4627963005c32ab2
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-05-10 11:50:11 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function App(props) {
 // very similar to append or appendChild
 render(
   <App cohort="43" instructor="Rhiannon" happy={true} birthday="October 31" />,
-  document.querySelector("#root")
+  document.querySelector("#x")
 );
 
 // we have many of these
