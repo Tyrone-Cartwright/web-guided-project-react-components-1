2642b456a4b55b05cfebca0aa3de8a86d7a7b753
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-05-10 12:27:40 -0500

    wip

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -15,7 +15,9 @@ export default function Playground(props) {
   // useState allows to us to set internal state
   // it is afunction that takes the desired initial state
   // it will return an array with 2 things: the state itself and a state changer
-  // NO declaring 
+  // NO declaring variables with 'let' to hold state => the hook takes its place
+
+  const [] = useState()
   return (
     <div className="container">
       <h3>Playground for Web {props.cohort}</h3>
